7c3f6c36aab3b856becd3b53721a39cf41494b55
Quasar Wei <quasar.wei@outlook.com>
2019-11-04 12:47:44 -0800

    finish through #7

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/* eslint-disable strict */
 //1st problem
 
 const sheepCounter = (sheepCount) => {
-  if(sheepCount === 0) {
+  if (sheepCount === 0) {
     console.log('All sheep jumped over the fence');
     return;
   }
@@ -21,7 +22,7 @@ const powerCalculator = (i, exponent) => {
   if (exponent < 0) {
     return 'exponent should be >= 0';
   }
-  
+
   return i * powerCalculator(i, exponent - 1);
 };
 
@@ -37,10 +38,62 @@ const reverseString = (string) => {
     return '';
   }
 
-  return string[string.length - 1]  + reverseString(string.slice(0, string.length - 1));
+  return string[string.length - 1] + reverseString(string.slice(0, string.length - 1));
 };
 
 const reverseStringAnswer = reverseString('elephant');
 console.log(reverseStringAnswer);
 
 //4th problem
+function triangularNumber(n) {
+  if (n === 1)
+    return n;
+
+  return n + triangularNumber(n - 1);
+}
+
+/* console.log(`triangularNumber: ${triangularNumber(3)}`);
+console.log(`triangularNumber: ${triangularNumber(4)}`);
+console.log(`triangularNumber: ${triangularNumber(5)}`);
+console.log(`triangularNumber: ${triangularNumber(6)}`);
+console.log(`triangularNumber: ${triangularNumber(7)}`);
+console.log(`triangularNumber: ${triangularNumber(8)}`); */
+
+// 5. string splitter
+function stringSplitter(string, separator) {
+  let index = string.indexOf(separator);
+
+  if (index === -1)
+    return [string];
+  
+  return [string.slice(0,index)].concat(
+    stringSplitter(
+      string.slice(index + separator.length), separator));
+}
+
+console.log(stringSplitter('20/20/2020', 's'));
+
+// 6. fibonacci
+function fibonacci(num) {
+  if (num===0)
+    return 'pick a number greater than or equal to 1'
+
+  if(num<=2)
+    return 1;
+
+  return fibonacci(num - 1) + fibonacci(num-2);
+}
+
+// 7. factorial
+function factorial(num) {
+  if(num===0) 
+    return 1;
+  if(num===1)
+    return 1;
+  return num * factorial(num-1);
+}
+
+
+// 8. maze
+
+// function maze()
