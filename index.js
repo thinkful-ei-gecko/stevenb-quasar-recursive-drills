728ac6eec7d1dddc2ff2c1b0e3d7a77e522726cf
Steven Bull <stevenwbulljr@gmail.com>
2019-11-04 17:15:13 +0000

    2nd problem solved

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1 problem
+//1st problem
 
 const sheepCounter = (sheepCount) => {
   if(sheepCount === 0) {
@@ -10,4 +10,21 @@ const sheepCounter = (sheepCount) => {
   return sheepCounter(sheepCount - 1);
 };
 
-sheepCounter(3);
+//sheepCounter(3);
+
+//2nd problem
+
+const powerCalculator = (i, exponent) => {
+  if (exponent < 1) {
+    console.log('exponent should be >= 0');
+    return;
+  }
+
+  console.log(i ** exponent);
+  powerCalculator(i, exponent - 1);
+};
+
+powerCalculator(10, 5);
+powerCalculator(10, -2);
+
+//3rd problem
